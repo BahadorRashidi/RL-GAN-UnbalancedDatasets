ad4d16f95cdbcae6f198fa8be314239c5527081a
BinYang24 <61919492+BinYang24@users.noreply.github.com>
2021-07-05 23:25:48 -0600

    Update ConditionalWGAN-GP.py

--- RL-GAN-FlowLevel-Intrusion/ConditionalWGAN-GP.py
+++ RL-GAN-FlowLevel-Intrusion/ConditionalWGAN-GP.py
@@ -22,9 +22,7 @@ from sklearn.model_selection import StratifiedShuffleSplit
 def setup_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    # torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
-    # random.seed(seed)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
